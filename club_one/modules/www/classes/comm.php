951e355e03bc9885efd812062159d3e4bb4e5ea1
Dragon Ling <dragonling@live.cn>
2013-04-02 01:25:17 +0800

    add comm

---
+++ club_one/modules/www/classes/comm.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Main Library.
+ *
+ * @package    www/Common
+ * @category   Common
+ * @author     Dragon
+ */
+class Comm extends Includes_Common{
+	
+}
